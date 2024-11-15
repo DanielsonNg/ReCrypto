c67e693b748b07186b07e029a1373c160ba0ddc6
Danielson <danielson7632@gmail.com>
2024-11-15 21:23:45 +0700

    material ui custom theme setup

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,14 +1,31 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import RootLayout from './layout/RootLayout'
+import { createTheme, colors, ThemeProvider } from '@mui/material'
+
+const theme = createTheme({
+  status: {
+    danger: '#e43e3e'
+  },
+  palette: {
+    secondary: {
+      main: colors.orange[500]
+    },
+    neutral:{
+      main: colors.grey[500],
+      darker: colors.grey[700],
+    },
+
+  }
+})
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Routes>
         <Route path='/' element={<RootLayout />} />
       </Routes>
-    </>
+    </ThemeProvider>
   )
 }
 
--- Frontend/src/layout/RootLayout.tsx
+++ Frontend/src/layout/RootLayout.tsx
@@ -1,7 +1,22 @@
+import { Box, Grid2 } from "@mui/material";
+import { styled } from '@mui/material'
+
+
+const StyledBox = styled(Box)(({ theme }) => ({
+    height:'250px',
+    width:'250px',
+    backgroundColor: theme.palette.neutral?.darker
+}))
+
 export default function RootLayout() {
     return (
         <>
-            <h1>TestLayout</h1>
+            <Box sx={{ bgcolor: 'secondary.dark' }}>
+                <h1>TestLayout</h1>
+            </Box>
+            <StyledBox>
+                <h1>gege</h1>
+            </StyledBox>
         </>
     )
 }
