9987501672542f35a602cb7b147bb54cad13ce12
Danielson <danielson7632@gmail.com>
2024-12-06 21:55:33 +0700

    chart with changable value

--- Frontend/src/pages/CoinPage.tsx
+++ Frontend/src/pages/CoinPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid2 } from "@mui/material"
+import { Box, Button, Grid2 } from "@mui/material"
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
@@ -18,20 +18,25 @@ type Chart = {
     total_volumes: [number[]]
 }
 
+type ChartType = 'price' | 'marketcap' | 'volume'
+
 export default function CoinPage() {
     const { id } = useParams()
     const [coin, setCoin] = useState<Coin | null>()
     const [chart, setChart] = useState<Chart | null>()
+    const [chartType, setChartType] = useState<ChartType>('price')
 
     useEffect(() => {
         (async () => {
             const response = await axios.get(`http://localhost:3333/coin?coinID=${id}`)
             setCoin(response.data.general)
             setChart(response.data.chart)
-            console.log(response.data.chart.market_caps)
         })()
     }, [])
 
+    const handleChartButton = (chartType: ChartType) => {
+        setChartType(chartType)
+    }
 
     return (
         <>
@@ -41,17 +46,69 @@ export default function CoinPage() {
                     <img style={{ width: '150px', height: '150px' }} src={coin.image}></img>
                     <h1>{coin.name}</h1> <h2 style={{ fontWeight: 'lighter' }}>{coin.symbol}</h2>
                 </Grid2>}
-                <Grid2>
-                    <Box sx={{ flexGrow: 1 }}>
-                        {chart ? <SparkLineChart data={chart.market_caps.map((data) => {
-                            return data[1]
-                        })} height={100} />
-                            : ''}
-                    </Box>
-                </Grid2>
                 {/* Mid */}
                 <Grid2>
-
+                    <Grid2 container direction={'row'} gap={3}>
+                        <Button variant="contained" color="success" onClick={() => handleChartButton('price')}>Price</Button>
+                        <Button variant="contained" color="success" onClick={() => handleChartButton('marketcap')}>MarketCap</Button>
+                        <Button variant="contained" color="success" onClick={() => handleChartButton('volume')}>Volume</Button>
+                    </Grid2>
+                    <Box sx={{ flexGrow: 1 }}>
+                        {chart ?
+                            chartType === 'price' ? <SparkLineChart
+                                height={100}
+                                colors={['green']}
+                                data=
+                                {chart.prices.map((data) => {
+                                    return parseFloat(data[1].toFixed(2).toString())
+                                })}
+                                xAxis={{
+                                    scaleType: 'time',
+                                    data:
+                                        chart.prices.map((data) => {
+                                            return new Date(data[0])
+                                        })
+                                }}
+                                showTooltip
+                                showHighlight
+                            />
+                            :
+                            chartType === 'marketcap' ? <SparkLineChart
+                                height={100}
+                                colors={['green']}
+                                data=
+                                {chart.market_caps.map((data) => {
+                                    return parseFloat(data[1].toFixed(2).toString())
+                                })}
+                                xAxis={{
+                                    scaleType: 'time',
+                                    data:
+                                        chart.prices.map((data) => {
+                                            return new Date(data[0])
+                                        })
+                                }}
+                                showTooltip
+                                showHighlight
+                            />
+                            :
+                            <SparkLineChart
+                                height={100}
+                                colors={['green']}
+                                data=
+                                {chart.total_volumes.map((data) => {
+                                    return parseFloat(data[1].toFixed(2).toString())
+                                })}
+                                xAxis={{
+                                    scaleType: 'time',
+                                    data:
+                                        chart.prices.map((data) => {
+                                            return new Date(data[0])
+                                        })
+                                }}
+                                showTooltip
+                                showHighlight
+                            /> : ''}
+                    </Box>
                 </Grid2>
                 {/* Bottom */}
                 <Grid2>
